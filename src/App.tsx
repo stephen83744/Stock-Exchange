f141b613e60881da3a74ac5949bfa2dc9d7be46c
Akinyemi Stephen <149060446+stephen83744@users.noreply.github.com>
2026-05-06 21:34:32 +0100

    Update imports and add new routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 
-import SignUp from "./SignupPage.js";
-import LoginPage from "./LoginPage.js";
+import SignUp from "./SignupPage.tsx";
+import LoginPage from "./LoginPage.tsx";
+import MarketNewsPage from "./marketNewsPage.tsx";
 import { Routes, Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+import TradePage from "./tradePage.tsx";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/loginPage" element={<LoginPage />} />
         <Route path="/" element={<SignUp />} />
+        <Route path ="/market" element={<MarketNewsPage />} />
+        <Route path="/trade" element={<TradePage />} />
       </Routes>
     </BrowserRouter>
   );
